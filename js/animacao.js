f52db5ed259d1b80a22b85f46eb134ab22bba8f2
Maicon Vasconcelos <118645080+maicoondev@users.noreply.github.com>
2022-12-02 10:13:13 -0300

    Add files via upload

---
+++ js/animacao.js
@@ -0,0 +1,10 @@
+/*MENU MOBILE*/
+document.querySelector(".abrirMenu").onclick = function(){
+    document.documentElement.classList.add("menuAtivo");
+}
+document.querySelector(".fecharMenu").onclick = function(){
+    document.documentElement.classList.remove("menuAtivo")
+}
+document.querySelector(".fechar").onclick = function(){
+    document.documentElement.classList.remove("menuAtivo");
+}
